f51e73d3bf92b46471de0f27161bad995c815825
mochi <minitomatoman1@gmail.com>
2025-06-19 11:25:24 +0900

    いったん

--- api/index.php
+++ api/index.php
@@ -1,3 +1,3 @@
 <?php
-<link rel="stylesheet" href="public/css/common.css"> <!-- view ディレクトリ内 -->
+<link rel="stylesheet" href="public/common.css"> <!-- view ディレクトリ内 -->
 <link rel="stylesheet" href="public/css/top.css"> <!-- view/public_html 内 -->
